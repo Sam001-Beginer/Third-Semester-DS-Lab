2848c9bd3db19f073968a5d650a56b53064832bb
Tsubasa <J16N@users.noreply.github.com>
2021-10-08 10:18:19 +0000

    Organized algorithms

--- Assignment-4/c/p3.c
+++ Assignment-4/c/p3.c
@@ -1,9 +1,22 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 int main(void)
 {
-    
+    printf("\n---------- EQUAL PARANTHESIS ----------\n");
+    printf("---------------------------------------\n");
 
+    int c;
+    printf("Expression: ");
+    int first = 0, second = 0, third = 0;
+    while ((c = getchar()) != '\n' && c != '\0' && c != EOF) {
+        if (c == '(') first++;
+        else if (c == '{') second++;
+        else if (c == '[') third++;
+        else if (c == ')' && first) first--;
+        else if (c == '}' && second) second--;
+        else if (c == ']' && third) third--;
+    }
+
+    printf("%s Parenthesis\n", first || second || third ? "Unequal" : "Equal");
     return 0;
 }
